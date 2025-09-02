4f25f6f55847cb0942fdad2482b944343fd769b0
Jean Barbosa <jeanbarbosa.it@gmail.com>
2025-09-02 20:34:32 -0300

    Implementando codigos python.

--- SABOR_EXPRESS/app.py
+++ SABOR_EXPRESS/app.py
@@ -3,3 +3,6 @@ print('1. Cadastrar Restaurante')
 print('2. Listar Restaurante')
 print('3. Ativar Restaurante')
 print('4. Sair Restaurante')
+
+opcao_escolhida = input('\n Escolha uma das opções: ')
+print('Você escolheu a opção',opcao_escolhida)
