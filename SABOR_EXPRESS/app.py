91c5cdc6aecfdbc9b9b480818d43f9d0f382e07a
Jean Barbosa <jeanbarbosa.it@gmail.com>
2025-09-02 20:51:34 -0300

    Implementando scripts python - menu de escolhas.

--- SABOR_EXPRESS/app.py
+++ SABOR_EXPRESS/app.py
@@ -1,8 +1,9 @@
-print('Sabor Express\n')
+print('\n Sabor Express \n')
+
 print('1. Cadastrar Restaurante')
 print('2. Listar Restaurante')
 print('3. Ativar Restaurante')
 print('4. Sair Restaurante')
 
-opcao_escolhida = input('\n Escolha uma das opções: ')
-print('Você escolheu a opção',opcao_escolhida)
+opcao_escolhida = input('Escolha uma das opções: ')
+print(f'Você escolheu a opção { opcao_escolhida}')
