64f9773c0a2c79dcc992f131be8f423ab0738c58
Jean Barbosa <jeanbarbosa.it@gmail.com>
2025-09-02 21:14:05 -0300

    Trabalhando com estrutura if, else e elif.

--- SABOR_EXPRESS/app.py
+++ SABOR_EXPRESS/app.py
@@ -5,5 +5,21 @@ print('2. Listar Restaurante')
 print('3. Ativar Restaurante')
 print('4. Sair Restaurante')
 
-opcao_escolhida = input('Escolha uma das opções: ')
-print(f'Você escolheu a opção { opcao_escolhida}')
+#opcao_escolhida = input('Escolha uma das opções: ')
+opcao_escolhida = int(input('Escolha uma das opções: '))
+
+#print(f'Você escolheu a opção { opcao_escolhida}')
+#print(opcao_escolhida == 1)
+#print(type(opcao_escolhida))
+#print(type(1))
+
+#opcao_escolhida= int(opcao_escolhida)
+
+if opcao_escolhida ==1:
+    print('Cadastrar Restaurantes')
+elif opcao_escolhida ==2:
+    print('Listar Restaurantes')    
+elif opcao_escolhida ==3:
+    print('Ativar Restaurantes')    
+else: 
+    print('Encerrando o programa.')
