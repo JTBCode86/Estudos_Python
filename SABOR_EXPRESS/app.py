d7b2d460a3fc7e695007dbd04aa8308d8e57af17
Jean Barbosa <jeanbarbosa.it@gmail.com>
2025-09-02 21:21:54 -0300

    Trabalhando com funções.

--- SABOR_EXPRESS/app.py
+++ SABOR_EXPRESS/app.py
@@ -1,3 +1,5 @@
+import os
+
 print('\n Sabor Express \n')
 
 print('1. Cadastrar Restaurante')
@@ -8,11 +10,14 @@ print('4. Sair Restaurante')
 #opcao_escolhida = input('Escolha uma das opções: ')
 opcao_escolhida = int(input('Escolha uma das opções: '))
 
+def finalizar_app():
+    os.system('cls')
+    print('Finalizando o app.\n')
+    
 #print(f'Você escolheu a opção { opcao_escolhida}')
 #print(opcao_escolhida == 1)
 #print(type(opcao_escolhida))
 #print(type(1))
-
 #opcao_escolhida= int(opcao_escolhida)
 
 if opcao_escolhida ==1:
@@ -21,5 +26,7 @@ elif opcao_escolhida ==2:
     print('Listar Restaurantes')    
 elif opcao_escolhida ==3:
     print('Ativar Restaurantes')    
-else: 
-    print('Encerrando o programa.')
+else:
+    #Implementar uma função no lugar do print. 
+    #print('Encerrando o programa.')
+    finalizar_app()
